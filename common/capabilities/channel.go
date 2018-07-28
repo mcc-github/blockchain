005521a6c5b1657eae43d52659b53a07112b275b
mcc-github <develop@m.cc>
2018-07-28 08:13:47 +0800

    blockchain_20180728_0813

--- common/capabilities/channel.go
+++ common/capabilities/channel.go
@@ -16,12 +16,16 @@ const (
 
 	
 	ChannelV1_1 = "V1_1"
+
+	
+	ChannelV1_3 = "V1_3"
 )
 
 
 type ChannelProvider struct {
 	*registry
 	v11 bool
+	v13 bool
 }
 
 
@@ -29,6 +33,7 @@ func NewChannelProvider(capabilities map[string]*cb.Capability) *ChannelProvider
 	cp := &ChannelProvider{}
 	cp.registry = newRegistry(cp, capabilities)
 	_, cp.v11 = capabilities[ChannelV1_1]
+	_, cp.v13 = capabilities[ChannelV1_3]
 	return cp
 }
 
@@ -41,6 +46,8 @@ func (cp *ChannelProvider) Type() string {
 func (cp *ChannelProvider) HasCapability(capability string) bool {
 	switch capability {
 	
+	case ChannelV1_3:
+		return true
 	case ChannelV1_1:
 		return true
 	default:
@@ -51,6 +58,8 @@ func (cp *ChannelProvider) HasCapability(capability string) bool {
 
 func (cp *ChannelProvider) MSPVersion() msp.MSPVersion {
 	switch {
+	case cp.v13:
+		return msp.MSPv1_3
 	case cp.v11:
 		return msp.MSPv1_1
 	default:
